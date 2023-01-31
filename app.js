3ef9188144839c1a3f34c5592bd560f5ac628fa3
Nicolas Olaya <111377816+NicolasAndrehh@users.noreply.github.com>
2023-01-31 14:25:01 -0500

    Install ESlint

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-let modalMenu = document.querySelector('#modal-menu')
-let openMenuButton = document.querySelector('#open-menu-button')
-let closeMenuButton = document.querySelector('#close-menu-button')
+const modalMenu = document.querySelector('#modal-menu');
+const openMenuButton = document.querySelector('#open-menu-button');
+const closeMenuButton = document.querySelector('#close-menu-button');
 
-openMenuButton.addEventListener("click", function(e) {
-    e.preventDefault()
-    modalMenu.classList.remove('hide')
-    modalMenu.classList.add('show')
-})
+openMenuButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  modalMenu.classList.remove('hide');
+  modalMenu.classList.add('show');
+});
 
-closeMenuButton.addEventListener("click", function(e) {
-    e.preventDefault()
-    modalMenu.classList.remove('show')
-    modalMenu.classList.add('hide')
-})
+closeMenuButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  modalMenu.classList.remove('show');
+  modalMenu.classList.add('hide');
+});
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener("click", function(e) {
-        e.preventDefault()
-        modalMenu.classList.remove('show')
-        modalMenu.classList.add('hide')
-        document.querySelector(this.getAttribute("href")).scrollIntoView({
-            behavior: "smooth"
-        })
-    })
-})
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener('click', (e) => {
+    e.preventDefault();
+    modalMenu.classList.remove('show');
+    modalMenu.classList.add('hide');
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
+});
